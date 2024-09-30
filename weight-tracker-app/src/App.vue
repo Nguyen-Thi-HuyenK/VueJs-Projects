0a4f0f476be770bb93ab8dc7441dc301357f87f9
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-09-30 09:10:55 +0300

    added weights list

--- weight-tracker-app/src/App.vue
+++ weight-tracker-app/src/App.vue
@@ -126,5 +126,164 @@ watch (weights, newWeights  => {
 </template>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+ }
 
+
+ body {
+   background: radial-gradient(879px at 10.4% 22.3%, rgb(121, 249, 249) 0%,    rgb(196, 232, 246) 93.6%);
+   min-height: 100vh;
+  }
+
+
+ #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  margin-top: 60px;
+ }
+
+
+  h1{
+  text-align: center;
+  font-size: 30px;
+  }
+
+
+  h2{
+  
+  text-align: center;
+  padding-top: 50px;
+  padding-bottom: 10px;
+  font-size: 20px;
+  color: black;
+  }
+
+
+  h3{
+  font-size: 25px;
+  padding: 5px 0;
+  }
+
+
+ .form{
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+  }
+
+
+  .input-number{
+  border: 1px solid #ddd;
+    border-radius: 2px;
+    width: 70%;
+    height: 40px;
+    padding: 10px 5px;
+    outline-color:rgb(249, 77, 126);
+    border: none;
+    appearance: none;
+    font-size: 17px;
+  }
+
+
+  .input-submit{
+    border: none;
+    border-radius: 3px;
+    height: 35px;
+    margin-top: 10px;
+    width: 50%;
+    cursor: pointer;
+    background-color: rgb(249, 77, 126);
+    outline-color: #fff;
+    color: #fff;
+
+
+  }
+
+
+ .current-container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  }
+
+
+  .current{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #fff;
+  margin: 30px 0;
+    border-radius: 50%;
+    width: 180px;
+    height: 180px;
+    border: 5px solid rgb(249, 77, 126);
+  }
+
+
+  main{
+  margin: 0;
+  padding: 20px;
+  }
+
+
+  .chart-section{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 2px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  margin: 10px 0;
+   }
+
+
+ 
+  main ul{
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  }
+
+
+ .weight-list{
+  border: 1px solid rgb(249, 77, 126);
+  border-radius: 5px;
+  background-color: #fff;
+  list-style: none;
+  text-align: center;
+  padding: 10px;
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px 0;
+  }
+
+
+ .weight-list span{
+  font-size: 18px;
+  font-weight: 700;
+ }
+
+
+ .weight-list small{
+  font-size: 15px;
+  font-weight: 600;
+  }
 </style>
