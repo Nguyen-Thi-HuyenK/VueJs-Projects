2a483d19393fa5957df417d0a6bc47511ce8a1f0
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-10-07 10:37:05 +0300

    setup completeted

--- fullstack-vue/src/server/index.ts
+++ fullstack-vue/src/server/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
 
 const app = express();
+app.get("/api/hi", (req, res) => res.send("Hello World!"));
 app.listen(3002, () => console.log('Server is running on port 3002'));
