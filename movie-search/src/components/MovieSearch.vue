4e43bb4528a95acf2fd419113b08595d792fbdb0
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-10-08 11:28:18 +0300

    initial commit

---
+++ movie-search/src/components/MovieSearch.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="search-bar">
+        <input
+            type="text"
+            placeholder="Movie name..."
+            class="search"
+            @input="searchInput"
+            @keypress.enter="search"
+        />
+
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
