abd32689fc02e65bedb075dadbec03bec7a8489a
Huyen Nguyen <c2ngth00@students.oamk.fi>
2024-10-08 10:21:58 +0300

    new

--- todo-vue/vite.config.ts
+++ todo-vue/vite.config.ts
@@ -8,6 +8,14 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: { proxy: { "/api": "http://localhost:3002" } },
+  esbuild: {
+    tsconfigRaw: {
+      compilerOptions: {
+        experimentalDecorators: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
